07fc8e9e2989b2a9968e69a809358218556c5b12
Rainy <1421436115@qq.com>
2024-07-03 06:57:55 +0800

    学习Git工具

---
+++ try_HF_240703.py
@@ -0,0 +1,12 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+# 非侵入式设置HuggingFace的下载镜像
+import os
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
+from transformers import pipeline
+
+# 指定模型版本  
+model_name = "bert-base-uncased@huggingface/bert-base-uncased-v2"  
+nlp = pipeline("sentiment-analysis", model=model_name)
